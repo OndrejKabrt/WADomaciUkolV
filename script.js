7d11740d1ad61a811bdbde32ff9100faa91b6074
Kivurawnuru <kabrtondra@gmail.com>
2024-01-03 20:48:12 +0100

    ThirdCommit

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
 let letters = "0123456789ABCDEF"; 
   
+var colours = [];
 
-
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
   
-function generujB(){
-    let color = '#'; 
-    for (let i = 0; i < 6; i++) {
-        color += letters[(Math.floor(Math.random() * 16))]; 
-        
+function generujB(number){
+    
+    for( let j = 0; j < number; j++){
+        let color = '#'; 
+        for (let i = 0; i < 6; i++) {
+            color += letters[(Math.floor(Math.random() * 16))]; 
+        }
+    colours.push(color);
     }
-    return color;
 }
 
+    generujB(3);
+    var index = 0;
+
     var button1 = document.getElementById("colour1");
-    button1.style.background = generujB();
+    button1.style.backgroundColor = colours[index];
     
     var button2 = document.getElementById("colour2");
-    button2.style.background = generujB();
+    button2.style.backgroundColor = colours[index+1];
     
     var button3 = document.getElementById("colour3");
-    button3.style.background = generujB();
+    button3.style.backgroundColor = colours[index+2];
+
+    
+    var h1 = colours[getRandomInt(3)];
+    document.getElementById("hledane").innerHTML = h1;
+    
+    
 
 
-var h1 = document.getElementById("hledane");
 
 
 
 
+/*
+var colours = [];
+  
+function coloursGenerator(number){
+    for( let i = 0; i < number; i++){
+        colours.push(generujB());
+    }
+}
 
+function columnsCreate(){
+    for( let i = 0; i < number; i++){
+        var ul = document.getElementById("colours");
+        var button = document.style.backgroundColor = colours[i];
+        ul.push(button)
+    }
+}
+*/
 
 
 
